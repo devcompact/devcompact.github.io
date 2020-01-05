a17b2a8ddcf9b350c0d4ca931d1539117360cabc
Igor Korolev <in.earth@ya.ru>
2020-01-05 14:07:37 +0300

    Fix sticky icon and date overlapping;

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Igor Korolev"  # (translatable)
 BLOG_TITLE = "Devcompactly"
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://devcompact.github.io"
+SITE_URL = "https://devcompact.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
